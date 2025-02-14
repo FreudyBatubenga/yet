bd0f0c1ad64b9a35ae143946a108a4d548e71cf0
Freudy Batubenga <fbatubenga@bulir.com>
2025-02-14 15:54:23 +0100

    fix

--- src/components/Centros.tsx
+++ src/components/Centros.tsx
@@ -1,41 +1,114 @@
 export function Centros() {
     return (
         <main className="w-[65%] min-h-screen items-center justify-center py-5 grid-cols-3 grid">
-            <div className="w-80 h-80 rounded-2xl border-2 "  style={{ backgroundImage: `url('/assets/image/Mask group.png')` }}>
-            <div className="w-full h-80 rounded-2xl bg-secbutton">
+            <div className="w-80 h-80 rounded-2xl "  style={{ backgroundImage: `url('/assets/image/Mask group.png')` }}>
+            <div className="w-full h-80 rounded-2xl bg-secbutton flex items-center flex-col justify-center gap-10">
 
+                <div className="w-72">
+                    <h1 className="text-2xl text-white font-bold">Centro de identificação civil e criminal </h1>
+                </div>
+                <div>
+                    <p className="text-bgbutton">Luanda, Sao Paulo</p>
+                    <span className="text-bgbutton">Seg - Sex / 08h - 15h</span>
+                </div>
+                <div className=" flex justify-between items-baseline w-full p-5">
+                    <span className="font-bold text-white" >476 <span className="font-extralight text-black">vagas disponiveis</span></span>
+                    <div className="w-11 h-11 bg-white rounded-xl">
+
+                    </div>
+                </div>
             </div>
             </div>
 
-            <div className="w-80 h-80 rounded-2xl border-2 "  style={{ backgroundImage: `url('/assets/image/Mask group.png')` }}>
-            <div className="w-full h-80 rounded-2xl bg-secbutton">
+            <div className="w-80 h-80 rounded-2xl "  style={{ backgroundImage: `url('/assets/image/Mask group.png')` }}>
+            <div className="w-full h-80 rounded-2xl bg-secbutton  flex items-center flex-col justify-center gap-10">
+            <div className="w-72">
+                    <h1 className="text-2xl text-white font-bold">Centro de identificação civil e criminal </h1>
+                </div>
+                <div>
+                    <p className="text-bgbutton">Luanda, Sao Paulo</p>
+                    <span className="text-bgbutton">Seg - Sex / 08h - 15h</span>
+                </div>
+                <div className=" flex justify-between items-baseline w-full p-5">
+                    <span className="font-bold text-white" >476 <span className="font-extralight text-black">vagas disponiveis</span></span>
+                    <div className="w-11 h-11 bg-white rounded-xl">
 
+                    </div>
+                </div>
             </div>
             </div>
 
   
 
-            <div className="w-80 h-80 rounded-2xl border-2"     style={{ backgroundImage: `url('/assets/image/Mask group.png')` }}  >
-            <div className="w-full h-80 rounded-2xl bg-secbutton">
+            <div className="w-80 h-80 rounded-2xl"     style={{ backgroundImage: `url('/assets/image/Mask group.png')` }}  >
+            <div className="w-full h-80 rounded-2xl bg-secbutton  flex items-center flex-col justify-center gap-10">
+            <div className="w-72">
+                    <h1 className="text-2xl text-white font-bold">Centro de identificação civil e criminal </h1>
+                </div>
+                <div>
+                    <p className="text-bgbutton">Luanda, Sao Paulo</p>
+                    <span className="text-bgbutton">Seg - Sex / 08h - 15h</span>
+                </div>
+                <div className=" flex justify-between items-baseline w-full p-5">
+                    <span className="font-bold text-white" >476 <span className="font-extralight text-black">vagas disponiveis</span></span>
+                    <div className="w-11 h-11 bg-white rounded-xl">
 
+                    </div>
+                </div>
             </div>
             </div>
 
-            <div className="w-80 h-80 rounded-2xl border-2"   style={{ backgroundImage: `url('/assets/image/Mask group.png')` }} >
-            <div className="w-full h-80 rounded-2xl bg-secbutton">
+            <div className="w-80 h-80 rounded-2xl"   style={{ backgroundImage: `url('/assets/image/Mask group.png')` }} >
+            <div className="w-full h-80 rounded-2xl bg-secbutton flex items-center flex-col justify-center gap-10">
+            <div className="w-72">
+                    <h1 className="text-2xl text-white font-bold">Centro de identificação civil e criminal </h1>
+                </div>
+                <div>
+                    <p className="text-bgbutton">Luanda, Sao Paulo</p>
+                    <span className="text-bgbutton">Seg - Sex / 08h - 15h</span>
+                </div>
+                <div className=" flex justify-between items-baseline w-full p-5">
+                    <span className="font-bold text-white" >476 <span className="font-extralight text-black">vagas disponiveis</span></span>
+                    <div className="w-11 h-11 bg-white rounded-xl">
 
+                    </div>
+                </div>
             </div>
             </div>
 
-            <div className="w-80 h-80 rounded-2xl border-2"   style={{ backgroundImage: `url('/assets/image/Mask group.png')` }}>
-            <div className="w-full h-80 rounded-2xl bg-secbutton">
+            <div className="w-80 h-80 rounded-2xl"   style={{ backgroundImage: `url('/assets/image/Mask group.png')` }}>
+            <div className="w-full h-80 rounded-2xl bg-secbutton flex items-center flex-col justify-center gap-10">
+            <div className="w-72">
+                    <h1 className="text-2xl text-white font-bold">Centro de identificação civil e criminal </h1>
+                </div>
+                <div>
+                    <p className="text-bgbutton">Luanda, Sao Paulo</p>
+                    <span className="text-bgbutton">Seg - Sex / 08h - 15h</span>
+                </div>
+                <div className=" flex justify-between items-baseline w-full p-5">
+                    <span className="font-bold text-white" >476 <span className="font-extralight text-black">vagas disponiveis</span></span>
+                    <div className="w-11 h-11 bg-white rounded-xl">
 
+                    </div>
+                </div>
             </div>
             </div>
 
-            <div className="w-80 h-80 rounded-2xl border-2"  style={{ backgroundImage: `url('/assets/image/Mask group.png')` }}>
-            <div className="w-full h-80 rounded-2xl bg-secbutton">
+            <div className="w-80 h-80 rounded-2xl"  style={{ backgroundImage: `url('/assets/image/Mask group.png')` }}>
+            <div className="w-full h-80 rounded-2xl bg-secbutton flex items-center flex-col justify-center gap-10">
+            <div className="w-72">
+                    <h1 className="text-2xl text-white font-bold">Centro de identificação civil e criminal </h1>
+                </div>
+                <div>
+                    <p className="text-bgbutton">Luanda, Sao Paulo</p>
+                    <span className="text-bgbutton">Seg - Sex / 08h - 15h</span>
+                </div>
+                <div className=" flex justify-between items-baseline w-full p-5">
+                    <span className="font-bold text-white" >476 <span className="font-extralight text-black">vagas disponiveis</span></span>
+                    <div className="w-11 h-11 bg-white rounded-xl">
 
+                    </div>
+                </div>
             </div>
             </div>
         </main>
